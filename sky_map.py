9184225b7d8e5c8807fd6e46007938dc0e464713
Kimberly <kowens4@elon.edu>
2025-04-14 17:28:41 -0400

    image draw

--- sky_map.py
+++ sky_map.py
@@ -10,3 +10,11 @@ eph = load('de421.bsp')
 observer = Topos(latittude_degrees=lat, longigtude_degrees=lon)
 earth = eph['earth']
 location = earth + observer
+
+#loading bright stars from HIpparcos catalog 
+
+with load.open('star_data/bright_stars.csv') as f:
+    lines = f.readlines()
+
+image = Image.new("RGB", (800,800), "black")
+draw = ImageDraw.draw(image)
