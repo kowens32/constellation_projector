eddacb4d485f0c2bb0c100627ed2385b05d1c081
Kimberly <kowens4@elon.edu>
2025-04-15 21:24:50 -0400

    updated display to only print on pi

--- display.py
+++ display.py
@@ -4,4 +4,8 @@ import os
 def show_image(image):
     path = "/tmp/star_map.png"
     image.save(path)
-    os.system(f"sud fbi -T 1 -d /dev/fb0 -noverbose -a {path}")
+
+    if platform.system() == "Linux" and "raspberrypi" in platform.uname().node:
+        os.system(f"sudo fbi -T 1 -d /dev/fb0 -noverbose -a {path}")
+    else:
+        print(f"[DEBUG] Skipping image display, not running on Pi.")
